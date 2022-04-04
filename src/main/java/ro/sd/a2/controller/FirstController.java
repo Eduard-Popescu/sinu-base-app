ba98b0ad57da4b0141679f3350f4d19803500ae2
Sopterean Andrei-Mihai <soptereanandrei@yahoo.com>
2022-04-04 22:45:26 +0300

    Populate db

--- src/main/java/ro/sd/a2/controller/FirstController.java
+++ src/main/java/ro/sd/a2/controller/FirstController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-import ro.sd.a2.entity.User;
 import ro.sd.a2.service.UserService;
 
 
@@ -16,19 +15,19 @@ public class FirstController {
 
     private UserService userService = new UserService();
 
-    @GetMapping("/profile")
-    public ModelAndView showProfile() {
-        //validation if needed
-        //shall we log a little?
-        ModelAndView mav = new ModelAndView();
-
-        User user = new User("Bubu");
-        mav.addObject("fullUserObj", user);
-        mav.addObject("numeStudent", user.getName());
-        // adaugi x obiecte
-        mav.setViewName("profile");
-        //log the final outcome: Success y?
-        return mav;
-    }
+//    @GetMapping("/profile")
+//    public ModelAndView showProfile() {
+//        //validation if needed
+//        //shall we log a little?
+//        ModelAndView mav = new ModelAndView();
+//
+//        User user = new User("Bubu");
+//        mav.addObject("fullUserObj", user);
+//        mav.addObject("numeStudent", user.getName());
+//        // adaugi x obiecte
+//        mav.setViewName("profile");
+//        //log the final outcome: Success y?
+//        return mav;
+//    }
 
 }
